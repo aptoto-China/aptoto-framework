ee8cbe596164e8f3e6f254df4a6d1af42d1b3e09
Administrator <Administrator@WIN-20170327ZAT>
2017-04-10 16:40:36 +0800

    数据备份方式接口

---
+++ pojo/src/main/java/com/aptoto/framework/BackupDeletedData.java
@@ -0,0 +1,11 @@
+package com.aptoto.framework;
+
+/**
+ * 数据删除前进行备份
+ * @author Orion
+ * @since 0.2-RELEASE
+ * 2017年4月10日
+ */
+public interface BackupDeletedData {
+
+}
---
+++ pojo/src/main/java/com/aptoto/framework/BackupUpdatedData.java
@@ -0,0 +1,11 @@
+package com.aptoto.framework;
+
+/**
+ * 数据修改前进行备份
+ * @author Orion
+ * @since 0.2-RELEASE
+ * 2017年4月10日
+ */
+public interface BackupUpdatedData {
+
+}
